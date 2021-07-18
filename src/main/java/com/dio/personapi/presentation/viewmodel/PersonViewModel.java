a0a8c1bc8fcd84b13ae7bc8968b0346f86e1d86e
Fabiano Bandeira <lfabianosb@gmail.com>
2021-07-18 19:08:45 -0300

    Format cpf field

--- src/main/java/com/dio/personapi/presentation/viewmodel/PersonViewModel.java
+++ src/main/java/com/dio/personapi/presentation/viewmodel/PersonViewModel.java
@@ -19,4 +19,11 @@ public class PersonViewModel {
   private String cpf;
   private String birthDate;
   private List<PhoneViewModel> phones = new ArrayList<>();
+
+  public String getCpf() {
+    if (this.cpf != null && this.cpf.length() == 11) {
+      return this.cpf.substring(0, 3) + "." + this.cpf.substring(3, 6) + "." + this.cpf.substring(6, 9) + "-" + this.cpf.substring(9);
+    }
+    return this.cpf;
+  }
 }
