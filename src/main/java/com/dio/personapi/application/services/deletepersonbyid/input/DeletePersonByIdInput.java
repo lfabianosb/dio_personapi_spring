7286df051702b27a2766303b8d34d29b0641b91c
Fabiano Bandeira <lfabianosb@gmail.com>
2021-07-19 12:47:02 -0300

    Delete person

---
+++ src/main/java/com/dio/personapi/application/services/deletepersonbyid/input/DeletePersonByIdInput.java
@@ -0,0 +1,12 @@
+package com.dio.personapi.application.services.deletepersonbyid.input;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+public class DeletePersonByIdInput {
+  private Long id;
+}
